c16e749db55563485275997240e34da2704abe15
marcos pinardi <exegeses@gmail.com>
2020-06-03 13:07:14 -0300

    condicionales + tarea

---
+++ inicial/clase1/variables.php
@@ -0,0 +1,11 @@
+<?php
+    $numero = 20;
+    $numero2 = 35;
+    echo 'El número es: ', $numero, ' y el número2 es: ', $numero2;
+?>
+<br>
+<?php
+    const NOMBRE = 'Rick';
+    const APELLIDO = 'Sánchez';
+    echo 'Tu nombre es: ', NOMBRE, ' ', APELLIDO;
+?>
