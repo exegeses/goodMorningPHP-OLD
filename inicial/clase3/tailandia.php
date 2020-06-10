9e78fd5272fb0007de3f626aa350006aa5d56881
marcos pinardi <exegeses@gmail.com>
2020-06-10 11:19:58 -0300

    materiales bucle tailandia

---
+++ inicial/clase3/tailandia.php
@@ -0,0 +1,59 @@
+<?php
+
+	$locacion = ["chinatown",
+					"floating-market", 
+					"hat-laem",
+					"wat-arun",
+					"khao-san-road",
+					"ko-khai-beach",
+					"kwai-river",
+					"limestone",
+					"longtail-boat",
+					"maya-bay",
+					"pra-nang",
+					"rai-leh-beach",
+					"rawai",
+					"sirocco",
+					"tuk-tuk",
+					"wat-saket"
+					 ];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tailandia</title>
+	<style type="text/css">
+		body{font-family: helvetica; color: #52BAE0}
+        #contenedor{
+            width: 800px;
+            margin: auto;
+        }
+		#contenedor article{
+			width:100px; 
+			padding:5px; 
+			margin:2px; 
+			display: inline-block;
+			border: 1px solid #ccc;
+		}
+		h1{padding-left:30px;font-size: 35px}
+		h2{font-size: 11px; color: #555;}
+	</style>
+</head>
+<body>
+
+	<h1>Tailandia</h1>
+    <section id="contenedor">
+
+        <article>
+            <img src="tailandia/muestra.png">
+            <br>
+            <h2>Locación</h2>
+        </article>
+
+    </section>
+
+</body>
+</html>
+
