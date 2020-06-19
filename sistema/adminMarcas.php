4db46315c60c62c4ff26bc33c7b73a2f98c07103
marcos pinardi <exegeses@gmail.com>
2020-06-19 11:32:47 -0300

    Panel de administración de marcas estático

---
+++ sistema/adminMarcas.php
@@ -0,0 +1,42 @@
+<?php  
+	
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Panel de administración de marcas</h1>
+
+        <table class="table table-bordered table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th>#</th>
+                    <th>Marca</th>
+                    <th colspan="2">
+                        <a href="" class="btn btn-dark">
+                            Agregar
+                        </a>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>id</td>
+                    <td>marca</td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary">
+                            Modificar
+                        </a>
+                    </td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary">
+                            Eliminar
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
