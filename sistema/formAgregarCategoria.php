ef7f304aa446e62308013276693602f9c95f2cec
marcos pinardi <exegeses@gmail.com>
2020-06-24 12:53:04 -0300

    validación de interfase "required" en el form.

--- sistema/formAgregarCategoria.php
+++ sistema/formAgregarCategoria.php
@@ -10,7 +10,7 @@
         <div class="alert bg-light border p-3 col-8 mx-auto">
             <form action="agregarCategoria.php" method="post">
                 Categoría: <br>
-                <input type="text" name="catNombre" class="form-control">
+                <input type="text" name="catNombre" class="form-control" required>
                 <br>
                 <button class="btn btn-dark">Agregar categoría</button>
                 <a href="adminCategorias.php" class="btn btn-outline-secondary">
