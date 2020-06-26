6762c0e22afc42a2e59a4cb006ad641e107359ae
marcos pinardi <exegeses@gmail.com>
2020-06-26 12:56:18 -0300

    función para agregar nuevo producto

--- sistema/funciones/productos.php
+++ sistema/funciones/productos.php
@@ -15,6 +15,59 @@
         return $resultado;
     }
 
+    function subirImagen()
+    {
+        /*** predeterminado si no enviaron archivo ***/
+        $prdImagen = 'noDisponible.jpg';
+
+        /*** si enviaron archivo y está ok  ***/
+        if( $_FILES['prdImagen']['error'] == 0 ){
+            ### subida del archivo
+            $ruta = 'productos/';
+            $temp = $_FILES['prdImagen']['tmp_name'];
+
+            ## renombrar archivo
+            # time() . extension
+            $path = pathinfo($_FILES['prdImagen']['name']);
+            $ext = $path['extension'];
+            $prdImagen = time().'.'.$ext;
+
+            ##subir archivo
+            move_uploaded_file( $temp, $ruta.$prdImagen );
+        }
+
+        return $prdImagen;
+    }
+
+    function agregarProducto()
+    {
+
+        $prdNombre = $_POST['prdNombre'];
+        $prdPrecio = $_POST['prdPrecio'];
+        $idMarca = $_POST['idMarca'];
+        $idCategoria = $_POST['idCategoria'];
+        $prdPresentacion = $_POST['prdPresentacion'];
+        $prdStock = $_POST['prdStock'];
+        $prdImagen = subirImagen();
+
+        $link = conectar();
+        $sql = "INSERT INTO productos
+                            (prdNombre, prdPrecio, idMarca, idCategoria, prdPresentacion, prdStock, prdImagen)
+                        VALUES 
+                            (
+                            '".$prdNombre."',
+                            ".$prdPrecio.",
+                            ".$idMarca.",
+                            ".$idCategoria.",
+                            '".$prdPresentacion."',
+                            ".$prdStock.",
+                            '".$prdImagen."'
+                            )";
+        $resultado = mysqli_query($link, $sql)
+                            or die( mysqli_error($link) );
+        return $resultado;
+    }
+
 ###################
 ### CRUD de productos
 /*
